3a1b583c2145c0e7d6efe964f225f152c8c49452
David Aspinall <daspinall@global-village.net>
2011-11-24 17:33:08 -0500

    Added MAC targets

---
+++ GVCFoundationOSXTests/GVCFoundationOSXTests.h
@@ -0,0 +1,13 @@
+//
+//  GVCFoundationOSXTests.h
+//  GVCFoundationOSXTests
+//
+//  Created by David Aspinall on 11-11-24.
+//  Copyright (c) 2011 Global Village Consulting. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface GVCFoundationOSXTests : SenTestCase
+
+@end
