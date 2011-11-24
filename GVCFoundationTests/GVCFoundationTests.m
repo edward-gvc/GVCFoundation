a1f1b4eba732378ba3f2b85408216b0b57aeb522
David Aspinall <daspinall@global-village.net>
2011-11-24 16:08:50 -0500

    Initial commit

--- GVCFoundationTests/GVCFoundationTests.m
+++ GVCFoundationTests/GVCFoundationTests.m
@@ -2,19 +2,56 @@
 //  GVCFoundationTests.m
 //  GVCFoundationTests
 //
-//  Created by David Aspinall on 11-11-24.
-//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//  Created by David Aspinall on 11-09-28.
+//  Copyright (c) 2011 Global Village Consulting Inc. All rights reserved.
 //
 
-#import "GVCFoundationTests.h"
+#import "GVCFoundation.h"
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+#pragma mark - Test Singleton class
+
+@interface GVCFoundation : NSObject
+{
+}
+GVC_SINGLETON_HEADER(GVCFoundation)
+@property (strong) NSString *uuid;
+@end
+
+@implementation GVCFoundation
+@synthesize uuid;
+GVC_SINGLETON_CLASS(GVCFoundation)
+
+/* init will initialize the UUID to test that it is only called once */
+- (id)init
+{
+    self = [super init];
+    if (self) {
+		NSAssert1(uuid == nil, @"uuid should be nil not %@", uuid);
+		[self setUuid:[NSString gvc_stringWithUUID]];
+    }
+    
+    return self;
+}
+@end
+
+#pragma mark - Test
+
+@interface GVCFoundationTests : SenTestCase
+{
+}
+@property (strong,nonatomic) NSString *origUUID;
+@end
 
 @implementation GVCFoundationTests
-
+@synthesize origUUID;
 - (void)setUp
 {
     [super setUp];
-    
-    // Set-up code here.
+		// set up the reference to the shared uuid here since we do not know what order the test methods will be called
+	[self setOrigUUID:[[GVCFoundation sharedGVCFoundation] uuid]];
 }
 
 - (void)tearDown
@@ -24,9 +61,28 @@
     [super tearDown];
 }
 
-- (void)testExample
+- (void)testSingleton
 {
-    STFail(@"Unit tests are not implemented yet in GVCFoundationTests");
+	GVCFoundation *shared = [GVCFoundation sharedGVCFoundation];
+	GVCFoundation *allocInit = [[GVCFoundation alloc] init];
+	NSString *allocUUID = [allocInit uuid];
+
+	STAssertEquals(shared, allocInit, @"Shared instances should be equal" );
+	STAssertFalse(gvc_IsEmpty(origUUID), @"UUID should be initialized in init '%@'", origUUID);
+	STAssertFalse(gvc_IsEmpty(allocUUID), @"UUID should be initialized in init '%@'", allocUUID);
+	
+	STAssertTrue([origUUID isEqual:allocUUID], @"UUID's changed after alloc/init %@ = %@", origUUID, allocUUID);
+    STAssertTrue( [[shared uuid] isEqual:[allocInit uuid]], @"%@ = %@", [shared uuid], [allocInit uuid]);
+}
+
+- (void)testSingletonScope
+{
+	GVCFoundation *shared = [GVCFoundation sharedGVCFoundation];
+	STAssertFalse(gvc_IsEmpty([shared uuid]), @"UUID should be initialized in init '%@'", [shared uuid]);
+	STAssertTrue([origUUID isEqual:[shared uuid]], @"UUID's changed after scope change %@ = %@", origUUID, [shared uuid]);
+
 }
 
 @end
+
+
